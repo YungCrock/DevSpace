958c4394cb10fb731ae7923fbd716f4d1446d60a
YungCrock <enzorodrigues164@gmail.com>
2026-04-18 20:50:12 -0300

    Add button animation

--- DevSpace/src/components/Button/MainButton.tsx
+++ DevSpace/src/components/Button/MainButton.tsx
@@ -1,15 +1,21 @@
 import { Pressable, Text, View } from "react-native";
 
+import { styles } from "./MainButtonStyles";
+
 export function MainButton() {
     return(
         <View>
-            <Pressable>
+            <Pressable
+                style={({ pressed }) => [styles.button, pressed && styles.pressedStyle, pressed && { transform: [{ scale: 0.97}] } ]}
+            >
                 <Text>
                     Sobre mim
                 </Text>
             </Pressable>
 
-            <Pressable>
+            <Pressable
+                style={({ pressed }) => [styles.button, pressed && styles.pressedStyle, pressed && { transform: [{ scale: 0.97}] } ]}
+            >
                 <Text>
                     Entre em contato!
                 </Text>
